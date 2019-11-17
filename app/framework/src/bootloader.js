c31b8d3c21f00f01699018ae42630462637896fe
Nashorn <jaysmith024@gmail.com>
2019-11-17 07:38:35 -0500

    bootloader simplified further

--- app/framework/src/bootloader.js
+++ app/framework/src/bootloader.js
@@ -3,37 +3,29 @@ import 'src/core/http/ModuleLoader.js';
 
 document.addEventListener("DOMContentLoaded", e => {
   async function bootup() {
-    var ns = Config.NAMESPACE||resolveNs()||'applications.AppEnvironment';
+    var ns = Config.NAMESPACE;//||resolveNs();//||'applications.AppEnvironment';
     if (Config.DYNAMICLOAD) {
       var filename_path = (
         Config.SRC_PATH + (ns.replace(/\./g, "/"))  + "/" + Config.FILENAME
       );
-
       var path = filename_path.replace("*", Config.USE_COMPRESSED_BUILD ? "min.":"");
       var c = (Config.ENABLE_TRANSPILER) ?
         new core.http.ClassLoader :
         new core.http.ModuleLoader;
-          c.load(ns, Config.ROOTPATH + path, data => init(ns));
+          c.load(ns, Config.ROOTPATH + path, data => {});
     }
-    else { init(ns); }
   };
 
-  function  resolveNs() {
-    var nsPath = location.href.split(Config.SRC_PATH)[1];
-    var parts  = nsPath?nsPath.split("/"):null;;
-        parts  = !parts||parts.length <=0?null:`${parts[0]}.${parts[1]}`;
-    return parts;
-  }
-
-  function init(ns) {
-    var ac = NSRegistry[ns];
-    (/\bApplicationContainer\b/.test(ns) && ac) ? new ac(document) : void(null)
-  }
+  // function  resolveNs() {
+  //   var nsPath = location.href.split(Config.SRC_PATH)[1];
+  //   var parts  = nsPath?nsPath.split("/"):null;;
+  //       parts  = !parts||parts.length <=0?null:`${parts[0]}.${parts[1]}`;
+  //   return parts;
+  // }
 
   ("cordova" in window) ? 
     document.addEventListener('deviceready', ()=>{
-      AndroidFullScreen.immersiveMode(function () { }, function () { });
+      AndroidFullScreen.immersiveMode(e=>{}, e=>{});
       bootup()
     }, false) : bootup()
-
 }, false);
