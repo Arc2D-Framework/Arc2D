36dc70f20c5308cca45edc82196dcbcaaf54387b
Nashorn <jaysmith024@gmail.com>
2019-11-23 07:27:21 -0500

    cleanup String extensions

--- app/framework/src/core/extensions/String.js
+++ app/framework/src/core/extensions/String.js
@@ -2,24 +2,6 @@
 if(!String.prototype.toDomElement){
     String.prototype.toDomElement = function(){
       var n = document.createRange().createContextualFragment(this.toString())
-      // if(!n) {console.error("#toDomElement() - Error parsing html",this.toString())}
       return n.firstElementChild;
     }
 };
-
-
-// String.prototype.toLocaleString = function(langCode){
-//   langCode = langCode||Session.State.currentLanguage.CODE;
-//   var key = this.toString();
-//   if(Localization){
-//     if(Localization[langCode]){
-//       return Localization[langCode][key]||
-//              Localization[langCode][key.toLowerCase()]||key;
-//     } else {
-//       return key;
-//     }
-//   }
-//   else {
-//     return key;
-//   }
-// };
