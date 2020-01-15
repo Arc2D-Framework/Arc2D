2b89e40c51738ddc28b087a98bb187449ff38718
Jason E. Smith <jaysmith024@gmail.com>
2020-01-15 13:03:21 -0500

    changed stylesheet life-cycle methods

--- src/docs/topics/ComponentNamespaces/index.js
+++ src/docs/topics/ComponentNamespaces/index.js
@@ -3,6 +3,25 @@ import '/src/docs/topics/Topic.js';
 namespace `docs.topics` (
     @cascade(true);
     class ComponentNamespaces extends docs.topics.Topic {
-        
+        onLoadStyle(url){
+        	console.log("onLoadStyle",url);
+        	return super.onLoadStyle(url);
+        }
+
+        onStylesheetLoaded(url){
+        	console.log("onStylesheetLoaded",url);
+        	return super.onStylesheetLoaded(url);
+        }
+
+        onTransformStyle(css){
+        	debugger;
+        	console.log("onTransformStyle",css);
+        	return super.onTransformStyle(css);
+        }
+
+        onAppendStyle(styletag){
+        	console.log("onAppendStyle",styletag);
+        	return super.onAppendStyle(styletag);
+        }
     }
 );
