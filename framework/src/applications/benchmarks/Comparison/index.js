45fc819f6ee7ee37f35d9186b08f1687d1ad9d73
Jason E. Smith <jaysmith024@gmail.com>
2020-03-01 08:51:37 -0500

    benchmark comparison app setup with chart.js

--- framework/src/applications/benchmarks/Comparison/index.js
+++ framework/src/applications/benchmarks/Comparison/index.js
@@ -3,12 +3,38 @@ namespace `applications.benchmarks` (
     @tag("benchmark-comparison");
     class Comparison extends w3c.ui.Application {
         async onConnected() {
-            await super.onConnected();
-            this.bind("#run-btn", "click", e=>this.onRunRequested(e))
+            await this.render();
+            this.draw()
         }
 
-        onRunRequested(){
-            alert("run")
+        draw(){
+            var ctx = this.querySelector('#myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Rendering', 'Sorting', 'Other'],
+                    datasets: [
+                        {
+                            label: 'Arc',
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [5, 2, 10]
+                        },
+                        {
+                            label: 'React',
+                            backgroundColor: 'rgb(155, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [5, 2, 10]
+                        },
+                        {
+                            label: 'Angular',
+                            backgroundColor: 'rgb(155, 200, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [5, 2, 10]
+                        }
+                    ]
+                }
+            });
         }
     }
 );
