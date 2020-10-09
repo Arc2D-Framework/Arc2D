b4e7230d72d403ede1d2af8e347106709139146d
jamesta696 <jaysmith696@gmail.com>
2020-10-08 22:26:06 -0700

    landing page updates - added hero slider prototype

--- src/display/screens/landing/MainScreen/index.js
+++ src/display/screens/landing/MainScreen/index.js
@@ -7,6 +7,18 @@ namespace `display.screens.landing` (
 
         async onConnected() {
             await super.onConnected();
+
+            // this.headingContainer = this.querySelector("#heading-container");
+            // this.heading1 = "<h1>An Agnostic, W3C/ES6 Compliant <div class='highlight'> 2D World & GUI Engine</div></h1><h5 style='margin-bottom: 33px;'>Native ES6 and HTML5 with time dependent simulation stepper for AI, DOM Physics and 2D Canvas games up to 60fps. No compilers, pre or post processors, no webpack, grunt or babel, no typescript or parsers</h5>";
+            // this.heading2 = "<h1>The Dawn of a New Age –<div class='highlight'> Arc2D</div> a Dynamic HTML W3C/ES6 Compliant Framework</h1><h5 style='margin-bottom: 33px;'>Arc2D is a dynamic HTML Framework used for its architecture, building apps, 2D games and more!</h5>";
+            // this.heading3 = "<h1>Lorem ipsum dolor sit atmet si <div class='highlight'> Lorem ipsum dolor sit</div></h1><h5 style='margin-bottom: 33px;'>Lorem ipsum dolor sit atmet siLorem ipsum dolor sit atmet siLorem ipsum dolor sit atmet siLorem ipsum dolor sit atmet siLorem ipsum dolor sit atmet siLorem ipsum dolor sit atmet siLorem ipsum dolor sit </h5>";
+            // this.headingsArray = [this.heading1, this.heading2, this.heading3];
+
+            // this.randomizeHeading();
         }
+
+        // randomizeHeading() {
+        //     this.headingContainer.innerHTML = this.headingsArray[Math.floor(Math.random() * this.headingsArray.length)];
+        // }
     }
 );
