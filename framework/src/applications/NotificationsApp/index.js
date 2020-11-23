c4b944fd7ebb7a2e6880d57bb2e501f54ba30db8
jamesta696 <jaysmith696@gmail.com>
2020-11-23 01:42:48 -0800

    new visual component engine pic

--- framework/src/applications/NotificationsApp/index.js
+++ framework/src/applications/NotificationsApp/index.js
@@ -1,7 +1,6 @@
 import 'node_modules/od-import-polyfill/import.js';
 await require('/src/core/drivers/templating/Nunjucks/nunjucks-driver.js');
 import 'core.ui.NotificationsToggleSwitch';
-//mport 'core.ui.Test2';
 
 namespace `applications` (
     class NotificationsApp extends w3c.ui.Application {
@@ -14,7 +13,9 @@ namespace `applications` (
         }
 
         async onConnected() {
-            await super.onConnected({items: ["News Feeds", "Likes and Comments", "Live Stream", "Upcoming Events", "Friend Requests", "Nearby Friends", "Birthdays", "Account Sign-In"]});
+            await super.onConnected({
+                items: ["News Feeds", "Likes and Comments", "Live Stream", "Upcoming Events", "Friend Requests", "Nearby Friends", "Birthdays", "Account Sign-In"]
+            });
             this.addEventListener("toggleit", (e) => this.toggleContent(e), false, "#main-toggle");
             this.bottomContent = this.querySelector("#bottom-content");
             this.allToggleSwitches = Array.from(this.querySelectorAll("notifications-toggle-switch"));
