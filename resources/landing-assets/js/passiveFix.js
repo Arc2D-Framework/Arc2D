868afa5485eaf4c8e6e377ac475d3ae5a0b76eaf
jamesta696 <jaysmith696@gmail.com>
2020-12-11 19:03:30 -0800

    fixed passive scroll + css render blockers

---
+++ resources/landing-assets/js/passiveFix.js
@@ -0,0 +1,13 @@
+(function () {
+    if (typeof EventTarget !== "undefined") {
+        let func = EventTarget.prototype.addEventListener;
+        EventTarget.prototype.addEventListener = function (type, fn, capture) {
+            this.func = func;
+            if(typeof capture !== "boolean"){
+                capture = capture || {};
+                capture.passive = false;
+            }
+            this.func(type, fn, capture);
+        };
+    };
+}());
