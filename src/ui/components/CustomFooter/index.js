234c9a41347cada5d6d1f324f5506c245916f88b
jamesta696 <jaysmith696@gmail.com>
2021-12-16 19:45:43 -0800

    updated demos: /demos/component-examples.html > new CustomFooter Component

---
+++ src/ui/components/CustomFooter/index.js
@@ -0,0 +1,11 @@
+namespace `ui.components` (
+	class CustomFooter extends WebComponent  {
+		constructor(element){
+            super(element);
+        }
+
+		async onConnected() {
+            await super.onConnected();
+        }
+	}
+)
--- src/ui/screens/ComponentsExample/index.js
+++ src/ui/screens/ComponentsExample/index.js
@@ -1,5 +1,6 @@
 import 'ui.components.CustomModal';
 import 'ui.components.ContextMenu';
+import 'ui.components.CustomFooter';
 
 namespace `ui.screens` (
     class ComponentsExample extends Application {
