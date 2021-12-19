2149dfebf1ab8891901a01c6acdbbe60eb351cff
jamesta696 <jaysmith696@gmail.com>
2021-12-18 18:36:15 -0800

    updated /demos/component-examples.html > ProductGrid UI

---
+++ src/ui/components/ProductGrid/index.js
@@ -0,0 +1,12 @@
+namespace `ui.components` (
+	class ProductGrid extends WebComponent  {
+		constructor(element){
+            super(element);
+        }
+
+        async onConnected() {
+            await super.onConnected();
+        }
+		
+	}
+)
--- src/ui/screens/ComponentsExample/index.js
+++ src/ui/screens/ComponentsExample/index.js
@@ -1,6 +1,7 @@
 import 'ui.components.CustomModal';
 import 'ui.components.ContextMenu';
 import 'ui.components.CustomFooter';
+import 'ui.components.ProductGrid';
 
 namespace `ui.screens` (
     class ComponentsExample extends Application {
@@ -11,5 +12,6 @@ namespace `ui.screens` (
         async onConnected() {
             await super.onConnected();
         }
+
     }
 );
