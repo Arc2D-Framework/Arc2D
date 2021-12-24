08b9682f36f54d9169de49ab49a17a76015e2bcc
Jason E. Smith <jaysmith024@gmail.com>
2021-12-24 08:22:17 -0500

    demos updated

--- src/ui/screens/ComponentsExample/index.js
+++ src/ui/screens/ComponentsExample/index.js
@@ -1,4 +1,8 @@
+import 'ui.components.CustomModal';
 import 'ui.components.GooeyNav';
+import 'ui.components.ContextMenu';
+import 'ui.components.CustomFooter';
+import 'ui.components.ProductGrid';
 
 namespace `ui.screens` (
     class ComponentsExample extends Application {
@@ -9,7 +13,7 @@ namespace `ui.screens` (
         async onConnected() {
             await super.onConnected();
             this.contextmenu = this.querySelector("ui-components-context-menu");
-            // this.on('contextmenu', (e) => this.onContextMenu(e), false);
+            this.on('contextmenu', (e) => this.onContextMenu(e), false, ".contextmenu-container");
             // this.on("mousedown", e => this.onToggleMenu(e))
         }
 
