7885c365a58894712509232df2182591080f07ae
jamesta696 <jaysmith696@gmail.com>
2022-01-13 22:21:20 -0800

    added cart item

--- src/ui/components/food/Cart/index.js
+++ src/ui/components/food/Cart/index.js
@@ -2,17 +2,57 @@ namespace `ui.components.food` (
 	class Cart extends Component  {
         async onConnected() {
             await super.onConnected();
-            application.on("request:additem", e => this.onRequestToAdd(e), false)
+            application.on("request:additem", e => this.onRequestToAdd(e), false);
+            this.cartItemsContainer = this.querySelector(".cart-items");
+            this.defaultCartText = this.querySelector(".cart-math p");
             this.items = [];
+            this.cartBasket = [];
         }
 
         onRequestToAdd(e){
             var item = e.data.item;
+            console.log(e.data.item);
             this.items.push(item);
             console.log("I now have these items", this.items);
+            this.onClearDefaultText();
+            this.onComputeCartItem(e);
             this.onCalculateSubTotal();
             this.onCalculateTax();
             this.onCalculateGrandTotal();
+        }
+
+        onComputeCartItem(e){
+            const title = e.data.item.lastElementChild.getAttribute('data-title');
+			const price = e.data.item.lastElementChild.getAttribute('data-price');
+			const imgLink = e.data.item.firstElementChild.attributes[0].nodeValue;
+
+            console.log({title}, "\n", {price}, "\n");
+
+            const cartItem = `
+				<li class="cart-item">
+                    <span class="fa fa-minus-circle" aria-hidden="true" style="visibility: hidden;"></span>
+					<img src="${imgLink}" alt="${title}">
+					<div class="cart-item-dets">
+						<p class="cart-item-heading">${title}</p>
+						<p class="g-price">$${price}</p>
+					</div>
+				</li>
+			`.toNode();
+
+            this.cartItemsContainer.appendChild(cartItem);
+            this.cartBasket.push(cartItem);
+            // console.log(this.cartBasket);
+            
+
+            // setTimeout(() => {
+            //     this.cartItemsContainer.removeChild(this.cartBasket.pop());
+            //     // this.cartBasket.pop();
+            //     console.log(this.cartBasket)
+            // }, 3000);
+        }
+
+        onClearDefaultText(){
+            this.defaultCartText.classList.add("hide");
         }
 
         onCalculateGrandTotal(){
@@ -32,13 +72,24 @@ namespace `ui.components.food` (
             this.querySelector("#subtotal").innerHTML = subtotal;
         }
 
+        onSubtractSubtotal(){
+
+        }
+
         onCalculateTax(){
             this.tax = (this.subtotal * 0.07)
             this.querySelector("#tax").innerHTML = this.tax.toFixed(2);
         }
 
+        onSubtractTax(){
+            //TODO
+        }
+
         onRequestToRemove(){
             //TODO
+            this.cartItemsContainer.removeChild(this.cartBasket.pop());
+            console.log(this.cartBasket)
+            this.onSubtractSubtotal();
         }
 	}
 )
