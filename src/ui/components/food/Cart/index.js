9c0a22cc3357ae094c3b894b55fe3a2be530f284
jamesta696 <jaysmith696@gmail.com>
2022-01-31 13:24:50 -0800

    cart logic to remove items

--- src/ui/components/food/Cart/index.js
+++ src/ui/components/food/Cart/index.js
@@ -8,9 +8,12 @@ namespace `ui.components.food` (
             this.defaultCartText = this.querySelector(".cart-math p");
             this.items = [];
             this.cartBasket = [];
+            this.subtotal = 0;
+            this.grandtotal = 0;
         }
 
         onRequestToAdd(e){
+            this._wasItemAdded = true;
             var item = e.data.item;
             this.items.push(item);
             console.log("I now have these items", this.items);
@@ -44,43 +47,56 @@ namespace `ui.components.food` (
 
         }
 
-        onClearDefaultText(){
-            this.defaultCartText.classList.add("hide");
-        }
-
-        onCalculateGrandTotal(){
-            var grand = this.subtotal + this.tax
-            this.querySelector("#grandtotal").innerHTML = grand.toFixed(2)
-        }
-
         onCalculateSubTotal(){
-            var subtotal = 0;
+            // var subtotal = 0;
             for(let item of this.items){
                 var price = item.querySelector(".g-price").textContent;
                     price = price.replace(/\$/g, ""); //remove $
                     price = parseFloat(price); //convert to a floating point number with decimals.
-                    subtotal +=price;
+                    // subtotal -=price;
+                    if(this._wasItemAdded){
+                        this.subtotal +=price;
+                    }else{
+                        this.subtotal -=price;
+                        
+                    }
             }
-            this.subtotal = subtotal;
-            this.querySelector("#subtotal").innerHTML = subtotal;
-        }
-
-        onSubtractSubtotal(){
-            // console.log("onSubtractSubtotal",this.items);
-            // for(let item of this.items){
-            //     var price = item.querySelector(".g-price").textContent;
-            //         price = price.replace(/\$/g, ""); //remove $
-            //         price = parseFloat(price); //convert to a floating point number with decimals.
-            //         subtotal -=price;
-            // }
             // this.subtotal = subtotal;
-            // console.log("SUBTOTAL:",this.subtotal)
-            // this.querySelector("#subtotal").innerHTML = this.subtotal;
+            this.querySelector("#subtotal").innerHTML = this.subtotal;
         }
 
         onCalculateTax(){
-            this.tax = (this.subtotal * 0.07)
+            var sum = (this.subtotal * 0.07);
+            if(this._wasItemAdded){
+                this.tax = sum;
+            }else{
+                this.tax = sum - sum;
+            }
+            // this.tax = (this.subtotal * 0.07);
+            console.log("THIS.TAX",this.tax.toFixed(2))
             this.querySelector("#tax").innerHTML = this.tax.toFixed(2);
+        }
+
+        onCalculateGrandTotal(){
+            if(this._wasItemAdded){
+                this.grandtotal = this.subtotal + this.tax;
+            }else{
+                this.grandtotal = this.subtotal - this.tax;
+            }
+            this.querySelector("#grandtotal").innerHTML = this.grandtotal.toFixed(2)
+        }
+
+        onSubtractSubtotal(e){
+            console.log("onSubtractSubtotal",this.items);
+            for(let item of this.items){
+                var price = item.querySelector(".g-price").textContent;
+                    price = price.replace(/\$/g, ""); //remove $
+                    price = parseFloat(price); //convert to a floating point number with decimals.
+                    this.subtotal -=price;
+            }
+            // this.subtotal = subtotal;
+            console.log("SUBTOTAL:",this.subtotal)
+            this.querySelector("#subtotal").innerHTML = this.subtotal;
         }
 
         onSubtractTax(e){
@@ -91,7 +107,8 @@ namespace `ui.components.food` (
             
         }
 
-        async onRequestToRemove(e){
+        onRequestToRemove(e){
+            this._wasItemAdded = false;
             console.log("this.items BEFORE REMOVE", this.items);
             const productNode = e.matchedTarget.parentNode;
             const filteredItem = this.items.filter(p =>{
@@ -99,11 +116,17 @@ namespace `ui.components.food` (
             })
             // this.onSubtractTax(e)
             // this.onSubtractSubtotal(e);
+            this.onCalculateSubTotal();
+            this.onCalculateTax();
+            this.onCalculateGrandTotal();
+
             this.items.splice(filteredItem,1);
             this.cartItemsContainer.removeChild(productNode);
             console.log("this.items AFTER REMOVE", this.items);
+        }
 
-            
+        onClearDefaultText(){
+            this.defaultCartText.classList.add("hide");
         }
 	}
 )
