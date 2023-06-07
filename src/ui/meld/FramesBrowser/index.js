adf9e8f699e62938e6b689e275b5621e23b7c4a4
JS <jaysmith024@gmail.com>
2023-06-07 18:20:35 -0400

    demos updated

--- src/ui/meld/FramesBrowser/index.js
+++ src/ui/meld/FramesBrowser/index.js
@@ -4,8 +4,25 @@ namespace `ui.meld` (
     class FramesBrowser extends WebComponent {
         async onConnected(){
             await super.onConnected();
+            this.onMouseMove = this.onMouseMove.bind(this);
             this.on("click", e=>this.onClick(e), true, "#frames li");
+            this.on("mousedown", e=> this.onMouseDown(e), false, ".drag-handle");
+            this.on("mouseup", e=> this.onMouseUp(e), false)
             this.setDefault();
+        }
+
+        onMouseUp(){
+            document.removeEventListener("mousemove", this.onMouseMove, false);
+            this.dispatchEvent("resize-pane-end", {delta})
+        }
+        onMouseDown(e){
+            this.xDelta = e.pageX;
+            document.addEventListener("mousemove", this.onMouseMove, false)
+        }
+
+        onMouseMove(e) {
+            var delta = e.pageX - this.xDelta;
+            this.dispatchEvent("resize-pane", {delta})
         }
 
         setDefault(){
--- src/ui/screens/Meld/index.js
+++ src/ui/screens/Meld/index.js
@@ -16,6 +16,22 @@ namespace `ui.screens` (
         
         async onConnected() {
             await super.onConnected();
+            this.nav = this.querySelector("#nav");
+            this.addEventListener("resize-pane", e => this.onResizeNav(e), false)
+            this.addEventListener("resize-pane-end", e => this.onResizeNavEnd(e), false)
+        }
+
+        onResizeNavEnd(){
+            
+        }
+        onResizeNav(e) {
+            // console.log("resize delta", e.detail.delta);
+            
+            const compStyles = window.getComputedStyle(this.nav);
+            var w = parseInt(compStyles.getPropertyValue("min-width"));
+            console.log("w",w)
+            // this.nav.style.width = w+e.detail.delta + "px";
+            this.nav.style.minWidth = (e.detail.delta) + "px"
         }
 
         inShadow(){return false}
