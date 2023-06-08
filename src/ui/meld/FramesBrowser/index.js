83dc6f76949e8e82283159a4fcd1e94b04ddaf4b
JS <jaysmith024@gmail.com>
2023-06-07 21:04:52 -0400

    demos updated

--- src/ui/meld/FramesBrowser/index.js
+++ src/ui/meld/FramesBrowser/index.js
@@ -8,21 +8,35 @@ namespace `ui.meld` (
             this.on("click", e=>this.onClick(e), true, "#frames li");
             this.on("mousedown", e=> this.onMouseDown(e), false, ".drag-handle");
             this.on("mouseup", e=> this.onMouseUp(e), false)
+            this.nav = document.querySelector("#nav");
             this.setDefault();
         }
 
+        // onResizeNav(e) {
+        //     const compStyles = window.getComputedStyle(this.nav);
+        //     var w = parseInt(compStyles.getPropertyValue("min-width"));
+        //     console.log("w",w)
+        //     this.nav.style.minWidth = (e.detail.delta) + "px"
+        // }
+
         onMouseUp(){
             document.removeEventListener("mousemove", this.onMouseMove, false);
-            this.dispatchEvent("resize-pane-end", {delta})
+            // this.dispatchEvent("resize-pane-end", {delta})
         }
         onMouseDown(e){
-            this.xDelta = e.pageX;
+            const compStyles = window.getComputedStyle(this.nav);
+            var w = parseInt(compStyles.getPropertyValue("min-width"),10);
+
+            this.startWidth = w;
+            this.startX = e.pageX;
             document.addEventListener("mousemove", this.onMouseMove, false)
         }
 
         onMouseMove(e) {
-            var delta = e.pageX - this.xDelta;
-            this.dispatchEvent("resize-pane", {delta})
+            var deltaX = e.pageX - this.startX;
+            var width = this.startWidth + deltaX;
+            this.nav.style.minWidth = `${width}px`
+            // this.dispatchEvent("resize-pane", {delta})
         }
 
         setDefault(){
@@ -37,6 +51,10 @@ namespace `ui.meld` (
             this.last && this.last.classList.remove("active");
             this.last=e.matchedTarget;
             this.last.classList.add("active")
+        }
+
+        inShadow(){
+            return true
         }
     }
 );
--- src/ui/screens/Meld/index.js
+++ src/ui/screens/Meld/index.js
@@ -17,22 +17,21 @@ namespace `ui.screens` (
         async onConnected() {
             await super.onConnected();
             this.nav = this.querySelector("#nav");
-            this.addEventListener("resize-pane", e => this.onResizeNav(e), false)
-            this.addEventListener("resize-pane-end", e => this.onResizeNavEnd(e), false)
+            // this.addEventListener("resize-pane", e => this.onResizeNav(e), false)
+            // this.addEventListener("resize-pane-end", e => this.onResizeNavEnd(e), false)
+            // var draghandle = this.querySelector(".FramesBrowser >>> .drag-handle")
+            // debugger
         }
 
-        onResizeNavEnd(){
-            
-        }
-        onResizeNav(e) {
-            // console.log("resize delta", e.detail.delta);
-            
-            const compStyles = window.getComputedStyle(this.nav);
-            var w = parseInt(compStyles.getPropertyValue("min-width"));
-            console.log("w",w)
-            // this.nav.style.width = w+e.detail.delta + "px";
-            this.nav.style.minWidth = (e.detail.delta) + "px"
-        }
+        // onResizeNavEnd(){
+
+        // }
+        // onResizeNav(e) {
+            // const compStyles = window.getComputedStyle(this.nav);
+            // var w = parseInt(compStyles.getPropertyValue("min-width"));
+        //     console.log("w",w)
+        //     this.nav.style.minWidth = (e.detail.delta) + "px"
+        // }
 
         inShadow(){return false}
     }
